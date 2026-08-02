9f840a047f621c0d65a8e40a846b4b9399f45d0d
dhth0111-ops <dhth0111@gmail.com>
2026-08-02 16:44:33 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE="yoriwang-v3.5-dev-fix4-20260731";
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./style.css?v=3.5-dev-fix4",
+  "./app.js?v=3.5-dev-fix4",
+  "./manifest.json?v=3.5-dev-fix4",
+  "./home-banner.png",
+  "./kkokkoma-icon-192-v34.png",
+  "./kkokkoma-icon-512-v34.png",
+  "./favicon-v34.png"
+];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  if(event.request.mode==="navigate"){
+    event.respondWith(fetch(event.request).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        if(response&&response.status===200){
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        }
+        return response;
+      })
+      .catch(()=>caches.match(event.request))
+  );
+});
